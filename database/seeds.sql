d1ddcf03c609cceab1c6fd973e900302893deb77
Don Tillery <dontillery@Dons-MacBook.local>
2018-06-24 18:55:26 -0400

    06-24

---
+++ database/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burgername, devoured)
+VALUES ("Bacon Burger", true);
+
+INSERT INTO burgers (burgername, devoured)
+VALUES ("Cheese Burger", false);
+
+INSERT INTO burgers (burgername, devoured)
+VALUES ("Double Cheese Burger", false);
+
+INSERT INTO burgers (burgername, devoured)
+VALUES ("Veggie Burger", true);
